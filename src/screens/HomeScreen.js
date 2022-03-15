844aa24204c26e64eb3b3712305dd269d0ecfb68
ohcnivek <64056046+ohcnivek@users.noreply.github.com>
2022-03-14 23:09:21 -0400

    some styling updates

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -23,19 +23,42 @@ function HomeScreen(props) {
     
     return (
         <div>
-            <h1>who has the worst groupme etiquette amongst ur friends? </h1>
-            <h5>to find out, get your groupme api developer token from this link, and submit in the box below! </h5>
-            <h5>also no, i dont keep track of tokens that are entered so dont worry :D</h5>
+            <header className="App-title">
+                <h1>who has the worst groupme etiquette? </h1>
+            </header>
+
+            <header className="App-header">
+                <h6> Disclaimer: I cannot see or save your access tokens in any way shape or form (at least to my knowledge) </h6>
+            </header>
+
+            <h5>Follow these simple steps: </h5> 
+
+            <header className="App-mid">
+
+            <h5>1. Login with your GroupMe credentials at this <a href="https://dev.groupme.com/session/new">link</a> </h5>
+            <h5>2. After verifying your identity etc, click the button labeled "Access Token" on the top right corner next to your name</h5>
+            <h5>3. Copy that access token, and paste it into the box below! </h5>
+
+            </header>
 
 
             <form onSubmit={handleSubmit(onSubmit)}>
                 <Controller
                 name="userAPIKey"
                 control={control}
+                defaultValue= {"paste token here"}
                 render={({ field }) => <Input {...field} />}
                 />
                 <input type="submit" />
             </form>
+
+
+
+
+
+            <h5>built with love, react + vercel, </h5>
+            <h5> - kevin c. </h5>
+
         </div>
     
     );
